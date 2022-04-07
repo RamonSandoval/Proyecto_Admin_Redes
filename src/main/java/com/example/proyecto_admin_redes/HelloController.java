ab104d186af2de5a558e22527f28299c0a292f5d
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-04-06 22:31:15 -0700

    Direcciones Ip
    Falta diseño, mostrar en interfaz, y realizar reporte

--- src/main/java/com/example/proyecto_admin_redes/HelloController.java
+++ src/main/java/com/example/proyecto_admin_redes/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.proyecto_admin_redes;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField txt_direccionIp;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick(ActionEvent event) {
+
+        InetAddress ping;
+        String ip = txt_direccionIp.getText(); // Ip de la máquina remota
+        try {
+            ping = InetAddress.getByName(ip);
+            if(ping.isReachable(3000)){
+                System.out.println(ip+" - Activo");
+            }else {
+                System.out.println(ip+" - no Activo");
+            }
+        } catch (IOException ex) { System.out.println(ex); }
+
     }
 }
+
---
+++ src/main/java/com/example/proyecto_admin_redes/Ping.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_admin_redes;
+
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Ping {
+    static TextField txt_direccionIp;
+        public static void main(String[] args) {
+
+        }
+
+}
