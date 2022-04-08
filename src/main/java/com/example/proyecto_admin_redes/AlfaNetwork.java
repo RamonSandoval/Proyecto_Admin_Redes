25d0b99c47f420435a4ac8cd5eb4910d63437ca3
rure0208 <79539402+rure0208@users.noreply.github.com>
2022-04-07 22:03:08 -0700

    prueba

--- src/main/java/com/example/proyecto_admin_redes/AlfaNetwork.java
+++ src/main/java/com/example/proyecto_admin_redes/AlfaNetwork.java
@@ -3,7 +3,6 @@ package com.example.proyecto_admin_redes;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +12,7 @@ public class AlfaNetwork extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AlfaNetwork.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 430, 282);
-        //stage.getIcons().add(new Image("/img/logo_AN.png"));
+        // stage.getIcons().add(new Image("/img/logo_AN.png"));
         stage.setTitle("Alfa Network Administrator");
         stage.setResizable(false);
         stage.setScene(scene);
--- src/main/java/com/example/proyecto_admin_redes/AlfaNetworkController.java
+++ src/main/java/com/example/proyecto_admin_redes/AlfaNetworkController.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
-import java.net.InetAddress;
+import java.net.*;
 
 public class AlfaNetworkController {
     @FXML
@@ -21,12 +21,13 @@ public class AlfaNetworkController {
     @FXML
     private Text textValidIP;
     @FXML
+    private Text textScann;
+    @FXML
     private Pane invalidIP;
     @FXML
     private Text textInvalidIP;
     @FXML
     private TextArea listIP;
-
 
     @FXML
     protected void onSearchClick(ActionEvent event) {
@@ -37,34 +38,39 @@ public class AlfaNetworkController {
         textValidIP.setText("");
         textInvalidIP.setText("");
 
-
-        if (direccionIp.getText() == ("")){
-           errorIP.setText("Ingrese una direccion IP");
-        }
-        else
-        try {
-            ping = InetAddress.getByName(ip);
-            if(ping.isReachable(3000)){
-                validIP.setVisible(true);
-                direccionIp.setText("");
-                listIP.appendText(ip +" - Activo\n");
-            }else {
-                listIP.appendText(ip+" - No Activo\n");
-                invalidIP.setVisible(true);
-
+        if (direccionIp.getText() == ("")) {
+            errorIP.setText("Ingrese una direccion IP");
+        } else
+            try {
+                ping = InetAddress.getByName(ip);
+                if (ping.isReachable(3000)) {
+                    validIP.setVisible(true);
+                    direccionIp.setText("");
+                    listIP.appendText(ip + " - Activo\n");
+                } else {
+                    listIP.appendText(ip + " - No Activo\n");
+                    invalidIP.setVisible(true);
+                }
+            } catch (IOException ex) {
+                System.out.println(ex);
             }
-        } catch (IOException ex) { System.out.println(ex); }
     }
+
     @FXML
-    protected void exitApp (ActionEvent event){
+    protected void Scann(ActionEvent event) throws UnknownHostException {
+
+    }
+
+    @FXML
+    protected void exitApp(ActionEvent event) {
         System.exit(0);
     }
+
     @FXML
-    protected void clearIP (ActionEvent event){
+    protected void clearIP(ActionEvent event) {
         direccionIp.setText("");
         listIP.setText("");
 
     }
 
 }
-
