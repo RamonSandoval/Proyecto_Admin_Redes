a3dd1b2a3023c101d3c911692d0f470786bde128
RamonSandoval <102700747+RamonSandoval@users.noreply.github.com>
2022-04-07 17:20:09 -0700

    Cambios generales
    App funcionando

--- src/main/java/com/example/proyecto_admin_redes/AlfaNetwork.java
+++ src/main/java/com/example/proyecto_admin_redes/AlfaNetwork.java
@@ -3,16 +3,18 @@ package com.example.proyecto_admin_redes;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AlfaNetwork extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(AlfaNetwork.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 430, 282);
+        stage.setTitle("Alfa Network Administrator");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/proyecto_admin_redes/AlfaNetworkController.java
@@ -0,0 +1,71 @@
+package com.example.proyecto_admin_redes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class AlfaNetworkController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private TextField direccionIp;
+    @FXML
+    private Text errorIP;
+    @FXML
+    private Pane validIP;
+    @FXML
+    private Text textValidIP;
+    @FXML
+    private Pane invalidIP;
+    @FXML
+    private Text textInvalidIP;
+    @FXML
+    private TextArea listIP;
+
+
+    @FXML
+    protected void onSearchClick(ActionEvent event) {
+        InetAddress ping;
+        String ip = direccionIp.getText(); // Direccion IP de la RD
+        validIP.setVisible(false);
+        invalidIP.setVisible(false);
+        textValidIP.setText("");
+        textInvalidIP.setText("");
+
+
+        if (direccionIp.getText() == ("")){
+           errorIP.setText("Ingrese una direccion IP");
+        }
+        else
+        try {
+            ping = InetAddress.getByName(ip);
+            if(ping.isReachable(3000)){
+                System.out.println(ip+" - Activo");
+                validIP.setVisible(true);
+                direccionIp.setText("");
+                textValidIP.setText("Activo");
+                listIP.appendText(ip +"\n");
+            }else {
+                System.out.println(ip+" - no Activo");
+                invalidIP.setVisible(true);
+                textInvalidIP.setText("Inactivo");
+            }
+        } catch (IOException ex) { System.out.println(ex); }
+    }
+    @FXML
+    protected void exitApp (ActionEvent event){
+        System.exit(0);
+    }
+    @FXML
+    protected void clearIP (ActionEvent event){
+        direccionIp.setText("");
+
+    }
+
+}
+
