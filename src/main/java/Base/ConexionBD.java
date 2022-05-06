f34193322333043f34948f93f258a31919f24274
rure0208 <79539402+rure0208@users.noreply.github.com>
2022-05-05 21:29:20 -0700

    con

---
+++ src/main/java/Base/ConexionBD.java
@@ -0,0 +1,16 @@
+package Base;
+
+
+public class ConexionBD {
+
+    static String bd = "admin_redes";
+    static String login = "root";
+    static String password = "";
+    static String url = "jdbc:mysql://localhost/" + bd;
+   
+
+    public ConexionBD() {
+      
+}
+
+}
