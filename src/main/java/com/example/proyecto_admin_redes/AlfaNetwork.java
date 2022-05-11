88c3f8886518c6f3e318480dc963651d1a1c45c3
RamonSandoval <102700747+RamonSandoval@users.noreply.github.com>
2022-05-10 22:01:30 -0700

    Interfaz Principal

--- src/main/java/com/example/proyecto_admin_redes/AlfaNetwork.java
+++ src/main/java/com/example/proyecto_admin_redes/AlfaNetwork.java
@@ -11,7 +11,7 @@ public class AlfaNetwork extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AlfaNetwork.class.getResource("admin.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 430, 282);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         // stage.getIcons().add(new Image("/img/logo_AN.png"));
         stage.setTitle("Alfa Network Administrator");
         stage.setResizable(false);
